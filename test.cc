7d0a2edcf53a4ce02b5eb524a76633cdf8f12fb1
asubah <abdulla.e.subah@gmail.com>
2020-02-20 11:15:27 +0100

    remove unwanted comments

--- test.cc
+++ test.cc
@@ -1,70 +1,13 @@
-//----------------------------------------------------------------------
-/// \file
-/// \page Example03 03 - using plugins
-///
-/// fastjet plugins example program:
-///   we illustrate the plugin usage
-///   here, we use the SISCone plugin though different choices are possible
-///   see the output of 'fastjet-config --list-plugins' for more details
-///
-/// Note that when using plugins, the code needs to be linked against
-/// the libfastjetplugins library (with the default monolithic
-/// build. For non-monolithic build, individual libraries have to be
-/// used for each plugin). 
-/// This is ensured in practice by calling
-///   fastjet-config --libs --plugins
-///
-/// run it with    : ./03-plugin < data/single-event.dat
-///
-/// Source code: 03-plugin.cc
-//----------------------------------------------------------------------
-
-//STARTHEADER
-// $Id: 03-plugin.cc 4354 2018-04-22 07:12:37Z salam $
-//
-// Copyright (c) 2005-2018, Matteo Cacciari, Gavin P. Salam and Gregory Soyez
-//
-//----------------------------------------------------------------------
-// This file is part of FastJet.
-//
-//  FastJet is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  The algorithms that underlie FastJet have required considerable
-//  development and are described in hep-ph/0512210. If you use
-//  FastJet as part of work towards a scientific publication, please
-//  include a citation to the FastJet paper.
-//
-//  FastJet is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with FastJet. If not, see <http://www.gnu.org/licenses/>.
-//----------------------------------------------------------------------
-//ENDHEADER
-
 #include "fastjet/ClusterSequence.hh"
 #include <iostream> // needed for io
 #include <cstdio>   // needed for io
 
-// include the SISCone plugin header if enabled
 #include "fastjet/config.h"
 #include "fastjet/GPUPlugin.hh"
-// #ifdef FASTJET_ENABLE_PLUGIN_SISCONE
-//#else
-//#warning "SISCone plugin not enabled. Skipping the example"
-//#endif // FASTJET_ENABLE_PLUGIN_SISCONE
-
 
 using namespace std;
 
 int main(){
-
-#ifdef FASTJET_ENABLE_PLUGIN_SISCONE
 
   // read in input particles
   //----------------------------------------------------------
@@ -81,19 +24,6 @@ int main(){
   // create a jet definition fron a plugin.
   // It basically requires declaring a JetDefinition from a pointer to
   // the plugin
-  //
-  // we will use the SISCone plugin here. Its (mandatory) parameters
-  // are a cone radius and an overlap threshold, plus other optional
-  // parameters
-  //
-  // for other plugin, see individual documentations for a description
-  // of their parameters
-  //
-  // the list of available plugins for a given build of FastJet can be
-  // obtained using
-  //   fastjet-config --list-plugins 
-  // from the command line.
-  //----------------------------------------------------------
   fastjet::GPUPlugin gpu(0.6, fastjet::antikt_algorithm);
   fastjet::JetDefinition jet_def(& gpu);
 
@@ -115,10 +45,10 @@ int main(){
   //    show the output as 
   //      {index, rap, phi, pt}
   //----------------------------------------------------------
-  // cout << "Ran " << jet_def.description() << endl;
+  cout << "Ran " << jet_def.description() << endl;
 
   // label the columns
-  // printf("%5s %15s %15s %15s\n","jet #", "rapidity", "phi", "pt");
+  printf("%5s %15s %15s %15s\n","jet #", "rapidity", "phi", "pt");
  
   // print out the details for each jet
   for (unsigned int i = 0; i < inclusive_jets.size(); i++) {
@@ -127,8 +57,5 @@ int main(){
 	   inclusive_jets[i].perp());
   }
 
-#endif
-
   return 0;
-
 }
